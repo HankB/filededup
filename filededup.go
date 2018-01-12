8135bd24595119ca8ce293bacfb3d82de8f76fc8
Hank Barta <hbarta@gmail.com>
2018-01-12 17:28:51 -0600

    refactor to support testing

--- filededup.go
+++ filededup.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto/md5"
 	"database/sql"
 	"fmt"
@@ -8,21 +9,21 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"bytes"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 /* return largest of two arguments
-*/
-func min( a,b int64) int64 {
+ */
+func min(a, b int64) int64 {
 	if a <= b {
 		return a
 	}
 	return b
 }
+
 /* Calculate the hash for the given file
-*/
+ */
 func getHash(filename string) []byte {
 	f, err := os.Open(filename)
 	if err != nil {
@@ -39,33 +40,33 @@ func getHash(filename string) []byte {
 var dbName = "filelist.db"
 var db *sql.DB
 var err error
-var hashes map[string][]byte 
+var hashes map[string][]byte
 
 /* Insert the file into the database
-*/
+ */
 func insertFile(filePath string, length int64, hash []byte) {
 	_, err = db.Exec(`insert into files (length, filename, hash) 
 				values(?, ?, ?)`, length, filePath, hash)
 	if err != nil {
-		log.Fatal(err);
-	} 
+		log.Fatal(err)
+	}
 }
 
 /* Update the hash for a file already in the database
-*/
+ */
 func updateHash(filePath string, hash []byte) {
 	fmt.Printf("updating hash %s %x\n", filePath, hash)
 	result, err := db.Exec(`update files set hash = ? where filename = ?`,
-			hash, filePath)
+		hash, filePath)
 	if err != nil {
-		log.Fatal(err);
+		log.Fatal(err)
 	} else {
 		rowCount, err := result.RowsAffected()
 		if err != nil {
-			log.Fatal(err);
+			log.Fatal(err)
 		} else {
 			if rowCount != 1 {
-				log.Fatal("hash update affected %d", rowCount);
+				log.Fatal("hash update affected %d", rowCount)
 			}
 		}
 	}
@@ -99,7 +100,7 @@ func compareByteByByte(f1, f2 string, len int64) bool {
 			log.Fatal("bytes read:", read1, " ", err)
 		}
 		if int64(read1) < min(blocksize, len-bytesRead) {
-			log.Fatal("Expected ",  min(blocksize, len-bytesRead), " bytes got ",  read1)
+			log.Fatal("Expected ", min(blocksize, len-bytesRead), " bytes got ", read1)
 		}
 
 		read2, err := file2.Read(buf2[0:min(blocksize, len-bytesRead)])
@@ -107,7 +108,7 @@ func compareByteByByte(f1, f2 string, len int64) bool {
 			log.Fatal(err)
 		}
 		if int64(read2) < min(blocksize, len-bytesRead) {
-			log.Fatal("Expected ",  min(blocksize, len-bytesRead), " bytes got ",  read2)
+			log.Fatal("Expected ", min(blocksize, len-bytesRead), " bytes got ", read2)
 		}
 
 		fmt.Printf("comparing bytes read:%d, %d: %d\n", read1, read2, bytes.Compare(buf1[0:read1], buf2[0:read2]))
@@ -119,7 +120,7 @@ func compareByteByByte(f1, f2 string, len int64) bool {
 }
 
 /* Check to see if a file matches (identical contents) to something
-   in the database. Update the hash for any files in the database that 
+   in the database. Update the hash for any files in the database that
    need to be checked and do not already have the hash calculated.
 */
 func findMatch(filepath string, length int64) (bool, string, []byte) {
@@ -136,12 +137,12 @@ func findMatch(filepath string, length int64) (bool, string, []byte) {
 
 	// check to see if any were found
 	more := rows.Next()
-	var hashCandidate  []byte = nil
-	if more { 
+	var hashCandidate []byte = nil
+	if more {
 		// found similar files - need to calculate the hash of the candidate
-		hashCandidate = getHash(filepath) // need hash 
+		hashCandidate = getHash(filepath) // need hash
 		for more {
-			var possMatchLen int64	// do we really need this?
+			var possMatchLen int64 // do we really need this?
 			var possMatchFilename string
 			var possMatchHash []byte
 			if err := rows.Scan(&possMatchLen, &possMatchFilename, &possMatchHash); err != nil {
@@ -161,7 +162,7 @@ func findMatch(filepath string, length int64) (bool, string, []byte) {
 		}
 	} else {
 		fmt.Printf("no length matches\n")
-		return false, "", nil	// no same length files
+		return false, "", nil // no same length files
 	}
 	if err := rows.Err(); err != nil {
 		log.Fatal(err)
@@ -175,23 +176,23 @@ func myWalkFunc(path string, info os.FileInfo, err error) error {
 		fmt.Printf("other: %s\n", path)
 	} else {
 		/*
-		hash := getHash(path)
-		fmt.Printf("len: %d name: %s hash %x\n", info.Size(), path, hash)
-		tx, err := db.Begin()
-		if err != nil {
-			log.Fatal(err)
-		}
+			hash := getHash(path)
+			fmt.Printf("len: %d name: %s hash %x\n", info.Size(), path, hash)
+			tx, err := db.Begin()
+			if err != nil {
+				log.Fatal(err)
+			}
 
-		stmt, err := tx.Prepare("insert into files(length, filename, hash) values(?, ?, ?)")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer stmt.Close()
-		_, err = stmt.Exec(info.Size(), path, hash)
-		if err != nil {
-			log.Fatal(err)
-		}
-		tx.Commit()
+			stmt, err := tx.Prepare("insert into files(length, filename, hash) values(?, ?, ?)")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer stmt.Close()
+			_, err = stmt.Exec(info.Size(), path, hash)
+			if err != nil {
+				log.Fatal(err)
+			}
+			tx.Commit()
 		*/
 		fmt.Printf("checking len: %d name: %s\n", info.Size(), path)
 		found, matchPath, hash := findMatch(path, info.Size())
@@ -205,15 +206,18 @@ func myWalkFunc(path string, info os.FileInfo, err error) error {
 	return nil
 }
 
-func main() {
-	os.Remove(dbName) // remove pevious copy
+/* init the database or go up in flames
+ */
+func initDataBase(flavor, location string) {
+	if location != ":memory:" {
+		os.Remove(location) // remove previous copy
+	}
 
 	// open the database
-	db, err = sql.Open("sqlite3", dbName)
+	db, err = sql.Open(flavor, location)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
 
 	sqlCreateStmt := `
 	create table files (
@@ -225,10 +229,17 @@ func main() {
 	// create the table
 	_, err = db.Exec(sqlCreateStmt)
 	if err != nil {
-		log.Printf("%q: %s\n", err, sqlCreateStmt)
-		return
+		log.Fatal(err)
 	}
+	return
+}
 
+func closeDataBase() {
+	db.Close()
+}
+func main() {
+	initDataBase("sqlite3", dbName)
+	defer closeDataBase()
 	filepath.Walk("./sample-files", myWalkFunc)
 
 }
