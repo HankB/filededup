068a2ef31e5e8cabe3eb344cb6c18a1e1c9e8c3e
Hank Barta <hbarta@gmail.com>
2018-01-12 11:47:16 -0600

    relocating source to filededup/ for testing

---
+++ filededup/filededup_test.go
@@ -0,0 +1,22 @@
+/*
+Copyright 2018 Hank Barta
+
+Test code for various routines in filededup.go
+
+*/
+
+
+package main
+
+import {
+	"testing"
+}
+
+func TestMin(t *testing.T)
+{
+	if min(3,4) == 4 {
+		t.Fatal("min() returned wrong value")
+	}
+ 
+
+}
