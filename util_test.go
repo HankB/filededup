cdc51ea5da472dffe93580040c716a16d8adf3f0
Hank Barta <hbarta@gmail.com>
2018-01-29 10:25:00 -0600

    test printf()

--- util_test.go
+++ util_test.go
@@ -39,3 +39,25 @@ func Example_psrseArgs() {
 	// 1 /somedir
 	// 1 /anotherdir
 }
+func Example_printf() {
+	options.Verbose = []bool{}
+	printf(priCritcl, "this is critical output\n")
+	printf(priInfo, "this is informational output\n")
+	printf(priWarn, "this is warning output\n")
+	options.Verbose = []bool{true}
+	printf(priCritcl, "this is critical output 2\n")
+	printf(priInfo, "this is informational output 2\n")
+	printf(priWarn, "this is warning output 2\n")
+	options.Verbose = []bool{true, true}
+	printf(priCritcl, "this is critical output 3\n")
+	printf(priInfo, "this is informational output 3\n")
+	printf(priWarn, "this is warning output 3\n")
+
+	// Output:
+	// this is critical output
+	// this is critical output 2
+	// this is warning output 2
+	// this is critical output 3
+	// this is informational output 3
+	// this is warning output 3
+}
