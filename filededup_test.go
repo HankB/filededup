a89dc59c2b8d5d0cac34dc3315528201aff5635e
Hank Barta <hbarta@gmail.com>
2018-01-29 07:41:26 -0600

    command line args

--- filededup_test.go
+++ filededup_test.go
@@ -189,3 +189,32 @@ func Example_findMatch() {
 	// 2|sample-files/x||1
 	// 2|sample-files/y||1
 }
+
+func Example_psrseArgs() {
+	os.Args = []string{"progname"}
+	parseArgs()
+	fmt.Println(len(options.Verbose), options.Directory)
+
+	os.Args = []string{"progname", "-v"}
+	parseArgs()
+	fmt.Println(len(options.Verbose), options.Directory)
+
+	os.Args = []string{"progname", "-vv"}
+	parseArgs()
+	fmt.Println(len(options.Verbose), options.Directory)
+
+	os.Args = []string{"progname", "--verbose", "-d", "/somedir"}
+	parseArgs()
+	fmt.Println(len(options.Verbose), options.Directory)
+
+	os.Args = []string{"progname", "-d", "/anotherdir"}
+	parseArgs()
+	fmt.Println(len(options.Verbose), options.Directory)
+
+	// Output:
+	// 0 .
+	// 1 .
+	// 2 .
+	// 1 /somedir
+	// 1 /anotherdir
+}
