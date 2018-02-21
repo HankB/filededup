e071da9f770383196505b26bb47821648ec174e1
Hank Barta <hbarta@gmail.com>
2018-02-20 22:07:21 -0600

    resolve conflicts between tests

--- util_test.go
+++ util_test.go
@@ -13,6 +13,10 @@ import (
 
 func checkArgs(args []string) {
 	os.Args = args
+	options.Verbose = nil
+	options.Directory = "."
+	options.Trial = false
+	options.Summary = false
 	parseArgs()
 	fmt.Println(setPrintfPri(priCritcl), options.Verbose, options.Directory, options.Trial, options.Summary)
 	options.Verbose = nil
