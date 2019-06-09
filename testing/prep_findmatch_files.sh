be4fcde58cc50435ba75cfdb37d3e9c10e99d0c5
Hank Barta <hbarta@gmail.com>
2019-06-08 20:16:48 -0500

    unlink test files

--- testing/prep_findmatch_files.sh
+++ testing/prep_findmatch_files.sh
@@ -7,3 +7,10 @@
 echo x > sample-files/z
 echo x > sample-files/x
 ln sample-files/x sample-files/y
+
+# accidentally linking some files (e.g run the executable in the project
+# directory) causes tests to fail. Fix that before proceeding
+
+cp sample-files/empty sample-files/tmp && mv sample-files/tmp sample-files/empty
+cp sample-files/'another file' sample-files/tmp && mv sample-files/tmp sample-files/'another file'
+exit 0
